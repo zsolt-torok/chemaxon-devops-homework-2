1f4806a9a92d81f446d9b79f853454e84165f03e
Zsolt Torok <62598970+zsolt-torok@users.noreply.github.com>
2024-06-27 00:12:26 +0200

    feat: implement access log bucket for backups (#2)

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,39 @@
-output "bucket_name" {
-  description = "The name of the created S3 bucket"
-  value       = aws_s3_bucket.content.bucket
+output "backup_bucket_name" {
+  description = "The name of the created backup S3 bucket"
+  value       = aws_s3_bucket.backup.bucket
 }
 
-output "bucket_arn" {
-  description = "The ARN of the created S3 bucket"
-  value       = aws_s3_bucket.content.arn
+output "backup_bucket_arn" {
+  description = "The ARN of the created backup S3 bucket"
+  value       = aws_s3_bucket.backup.arn
 }
 
-output "bucket_region" {
-  description = "The AWS region of the created S3 bucket"
-  value       = aws_s3_bucket.content.region
+output "backup_bucket_region" {
+  description = "The AWS region of the created backup S3 bucket"
+  value       = aws_s3_bucket.backup.region
+}
+
+output "log_bucket_name" {
+  description = "The name of the created log S3 bucket"
+  value       = aws_s3_bucket.log.bucket
+}
+
+output "log_bucket_arn" {
+  description = "The ARN of the created log S3 bucket"
+  value       = aws_s3_bucket.log.arn
+}
+
+output "log_bucket_region" {
+  description = "The AWS region of the created log S3 bucket"
+  value       = aws_s3_bucket.log.region
 }
 
 output "kms_key_id" {
-  description = "The ID of the KMS key used for S3 bucket encryption"
+  description = "The ID of the KMS key used for S3 backup bucket encryption"
   value       = aws_kms_key.this.id
 }
 
 output "kms_key_arn" {
-  description = "The ARN of the KMS key used for S3 bucket encryption"
+  description = "The ARN of the KMS key used for S3 backup bucket encryption"
   value       = aws_kms_key.this.arn
 }
